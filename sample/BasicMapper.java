4951a862e955be0e5eb42362b85f293da3700856
downy <tkwk36@naver.com>
2024-06-25 18:19:32 +0900

    .

--- sample/BasicMapper.java
+++ sample/BasicMapper.java
@@ -1,1 +1,17 @@
-// >
+// package com.aloha.server.mapper;
+
+// import java.util.List;
+
+// import org.apache.ibatis.annotations.Mapper;
+
+// import com.aloha.server.dto.Board;
+// import com.aloha.server.dto.Files;
+
+// @Mapper
+// public interface BasicMapper {
+//     public List<Files> list() throws Exception;
+//     public Files select(int no) throws Exception;
+//     public int insert(Files file) throws Exception;
+//     public int update(Board file) throws Exception;
+//     public int delete(int no) throws Exception;
+// }
